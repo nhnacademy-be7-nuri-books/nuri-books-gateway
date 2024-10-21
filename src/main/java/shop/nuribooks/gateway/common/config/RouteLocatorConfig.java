f85cf7c7a97b0aa3f8118ef2c14a0d3fc9ad2b5f
yeon-so <nordstromm03@gmail.com>
2024-10-21 21:51:10 +0900

    [FEAT] Gateway route 설정 추가
    member - /api/member/**
    auth - /api/auth/**
    books
     - /api/books/**
     - /api/categories/**
     - /api/authors/**
     - /api/publishers/**
    view - /api/view/**

---
+++ src/main/java/shop/nuribooks/gateway/common/config/RouteLocatorConfig.java
@@ -0,0 +1,37 @@
+package shop.nuribooks.gateway.common.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteLocatorConfig {
+
+	@Bean
+	public RouteLocator myRoute(RouteLocatorBuilder builder) {
+
+		return builder.routes()
+			.route("member",
+				p -> p.path("/api/member/**").uri("lb://member")
+			)
+			.route("auth",
+				p -> p.path("/api/auth/**").uri("lb://auth")
+			)
+			.route("books",
+				p -> p.path("/api/books/**")
+					.or()
+					.path("/api/categories/**")
+					.or()
+					.path("/api/authors/**")
+					.or()
+					.path("/api/publishers/**")
+					.uri("lb://books")
+			)
+			.route("view",
+				p -> p.path("/api/view/**").uri("lb://view")
+			)
+			.build();
+
+	}
+}

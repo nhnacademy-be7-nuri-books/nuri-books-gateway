d608bd1cc5ccb33acc0107ea37bd31fa338cb2da
yeon-so <nordstromm03@gmail.com>
2024-10-24 09:45:49 +0900

    refactor: router 주석 추가

--- src/main/java/shop/nuribooks/gateway/common/config/RouteLocatorConfig.java
+++ src/main/java/shop/nuribooks/gateway/common/config/RouteLocatorConfig.java
@@ -5,16 +5,22 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Gateway router
+ * @author : nuri
+ */
 @Configuration
 public class RouteLocatorConfig {
 
+	/**
+	 * 라우트 설정
+	 * @param builder RouteLocatorBuilder 객체
+	 * @return 설정된 RouteLocator 객체
+	 */
 	@Bean
 	public RouteLocator myRoute(RouteLocatorBuilder builder) {
 
 		return builder.routes()
-			.route("auth",
-				p -> p.path("/api/auth/**").uri("lb://auth")
-			)
 			.route("books",
 				p -> p.path("/api/books/**")
 					.or()
@@ -29,6 +35,10 @@ public class RouteLocatorConfig {
 			)
 			.route("view",
 				p -> p.path("/api/view/**").uri("lb://view")
+			)
+			.route("auth",
+				p -> p.path("/api/auth/**")
+					.uri("lb://auth")
 			)
 			.build();
 	}

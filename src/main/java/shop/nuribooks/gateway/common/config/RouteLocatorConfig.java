5a81ae28b84e8c8f7168db9a827bd235d0f5abee
yeon-so <nordstromm03@gmail.com>
2024-10-27 23:50:18 +0900

    fix: 필터 사용을 위한 코드 수정

--- src/main/java/shop/nuribooks/gateway/common/config/RouteLocatorConfig.java
+++ src/main/java/shop/nuribooks/gateway/common/config/RouteLocatorConfig.java
@@ -5,12 +5,23 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import shop.nuribooks.gateway.common.filter.MemberJwtFilter;
+import shop.nuribooks.gateway.common.filter.SignupFilter;
+
 /**
  * Gateway router
  * @author : nuri
  */
 @Configuration
 public class RouteLocatorConfig {
+
+	private final MemberJwtFilter memberJwtFilter;
+	private final SignupFilter signupFilter;
+
+	public RouteLocatorConfig(MemberJwtFilter memberJwtFilter, SignupFilter signupFilter) {
+		this.memberJwtFilter = memberJwtFilter;
+		this.signupFilter = signupFilter;
+	}
 
 	/**
 	 * 라우트 설정
@@ -21,20 +32,34 @@ public class RouteLocatorConfig {
 	public RouteLocator myRoute(RouteLocatorBuilder builder) {
 
 		return builder.routes()
-			.route("books",
+			.route("books_route",
 				p -> p.path("/api/books/**")
-					.or()
-					.path("/api/categories/**")
-					.or()
-					.path("/api/authors/**")
-					.or()
-					.path("/api/publishers/**")
-					.or()
-					.path("/api/member/**")
 					.uri("lb://books")
 			)
-			.route("view",
-				p -> p.path("/api/view/**").uri("lb://view")
+			.route("categories_route",
+				p -> p.path("/api/categories/**")
+					.uri("lb://books")
+			)
+			.route("authors_route",
+				p -> p.path("/api/authors/**")
+					.uri("lb://books")
+			)
+			.route("publishers_route",
+				p -> p.path("/api/publishers/**")
+					.uri("lb://books")
+			)
+			.route("member_route",
+				p -> p.path("/api/member")
+					.filters(f -> f.filter(signupFilter.apply(new SignupFilter.Config())))
+					.uri("lb://books")
+			)
+			.route("member_route",
+				p -> p.path("/api/member/**")
+					.uri("lb://books")
+			)
+			.route("auth",
+				p -> p.path("/api/auth/login")
+					.uri("lb://auth")
 			)
 			.route("auth",
 				p -> p.path("/api/auth/**")

3ff4b6b9b85d91f0059289d98f7635780a2d1673
yeon-so <115128881+yeon-so@users.noreply.github.com>
2024-11-04 13:13:29 +0900

    [feature/gateway] gateway 기본 설정 (#1)
    * [FEAT] Gateway route 설정 추가
    
    member - /api/member/**
    auth - /api/auth/**
    books
     - /api/books/**
     - /api/categories/**
     - /api/authors/**
     - /api/publishers/**
    view - /api/view/**
    
    * [FIX] member & books 통합으로 gateway 정보 변경
    
    * [FIX] member & books 통합으로 gateway 정보 변경 2
    
    * refactor: router 주석 추가
    
    * feat: 설정 파일 cors 정보 추가
    
    * feat: 회원 가입 시 비밀번호를 암호화하는 필터 추가
    
    * feat: 회원 가입 시 요청 본문을 재작성하는 필터 추가
    
    * feat: 암호화 모듈 사용을 위한 filter 추가
    
    * feat: gateway 로 요청/응답온 메서드를 표시해주는 로그 필터 추가
    
    * feat: jwt 검증을 위한 jwtUtil 추가
    
    * feat: cors 설정 추가
    
    * fix: 필터 사용을 위한 코드 수정
    
    * feat: 로그인 필터 추가
    
    * fix : jwt 정보 수정
    
    * feat : spring WebClient 설정 파일 추가
    
    * feat : 토큰 재갱신 검증 필터 추가
    
    * feat : jwt 검증 메소드 추가
    
    * fix : gateway 라우터 수정
    
    * refactor: 일부 문자열 설정 파일 변수로 변경
    
    * chore: 일부 설정 값 변경
    
    * fix : x-user-id, x-user-role 요청 헤더에서 추가 및 라우터 설정 수정

---
+++ src/main/java/shop/nuribooks/gateway/common/config/RouteLocatorConfig.java
@@ -0,0 +1,66 @@
+package shop.nuribooks.gateway.common.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import shop.nuribooks.gateway.common.filter.GlobalJwtValidationFilter;
+import shop.nuribooks.gateway.common.filter.LoginFilter;
+import shop.nuribooks.gateway.common.filter.SignupFilter;
+
+/**
+ * Gateway router
+ * @author : nuri
+ */
+@Configuration
+public class RouteLocatorConfig {
+
+	private final GlobalJwtValidationFilter globalJwtValidationFilter;
+	private final LoginFilter loginFilter;
+	private final SignupFilter signupFilter;
+
+	public RouteLocatorConfig(GlobalJwtValidationFilter globalJwtValidationFilter, LoginFilter loginFilter,
+		SignupFilter signupFilter) {
+		this.globalJwtValidationFilter = globalJwtValidationFilter;
+		this.loginFilter = loginFilter;
+		this.signupFilter = signupFilter;
+	}
+
+	/**
+	 * 라우트 설정
+	 * @param builder RouteLocatorBuilder 객체
+	 * @return 설정된 RouteLocator 객체
+	 */
+	@Bean
+	public RouteLocator myRoute(RouteLocatorBuilder builder) {
+
+		return builder.routes()
+			.route("books_route",
+				p -> p.path("/api/books/**", "/api/categories/**", "/api/contributors/**", "/api/reviews/**",
+						"/api//**")
+					.filters(f -> f.filter(globalJwtValidationFilter.apply(new GlobalJwtValidationFilter.Config())))
+					.uri("lb://books")
+			)
+			.route("member_route",
+				p -> p.path("/api/member")
+					.filters(f -> f.filter(signupFilter.apply(new SignupFilter.Config())))
+					.uri("lb://books")
+			)
+			.route("member_route",
+				p -> p.path("/api/member/**")
+					.filters(f -> f.filter(globalJwtValidationFilter.apply(new GlobalJwtValidationFilter.Config())))
+					.uri("lb://books")
+			)
+			.route("auth",
+				p -> p.path("/api/auth/login")
+					.filters(f -> f.filter(loginFilter.apply(new LoginFilter.Config())))
+					.uri("lb://auth")
+			)
+			.route("auth",
+				p -> p.path("/api/auth/**")
+					.uri("lb://auth")
+			)
+			.build();
+	}
+}
---
+++ src/main/java/shop/nuribooks/gateway/common/filter/LogFilter.java
@@ -0,0 +1,40 @@
+package shop.nuribooks.gateway.common.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+/**
+ * gateway 로 요청/응답온 메서드를 표시해주는 로그 필터
+ * @author : nuri
+ */
+@Component
+@Slf4j
+public class LogFilter implements GlobalFilter {
+
+	/**
+	 * 게이트웨이를 통한 요청이 올 시 해당 요청과 관련된 로그 조회
+	 *
+	 * @param exchange 요청 및 응답 정보
+	 * @param chain 다음 필터 체인
+	 * @return 필터 적용 결과
+	 */
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+		String method = exchange.getRequest().getMethod().name();
+		String requestPath = exchange.getRequest().getURI().toString();
+
+		log.info("request 정보: Method - {}, Path - {}", method, requestPath);
+
+		return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+			int statusCode = exchange.getResponse().getStatusCode() != null
+				? exchange.getResponse().getStatusCode().value() : 0;
+			log.info("response 정보: Status - {}", statusCode);
+		}));
+	}
+}
